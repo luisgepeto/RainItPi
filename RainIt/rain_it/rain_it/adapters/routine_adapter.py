e7934639598c01e0de8377df234140b22626d112
Luis Becerril <luisgepeto@gmail.com>
2015-08-05 16:16:59 -0500

    moved logic to make get and post calls with token

--- RainIt/rain_it/rain_it/adapters/routine_adapter.py
+++ RainIt/rain_it/rain_it/adapters/routine_adapter.py
@@ -1,15 +1,12 @@
 '''
 This file will have the methods defined for calling the routine methods
 '''
-import requests
+
 from common import adapter_util
 from domain.routine_response import Routine
 
 def get_active_routines(token):
-    url = "http://localhost:8723/api/routine/allActive"
-    authorization_header = adapter_util.get_authorization_header(token)
-    r = requests.get(url, headers= authorization_header)
-    json_result = r.json()
+    json_result = adapter_util.make_get_service_call("http://localhost:8723/api/routine/allActive", token)
     routine_list = []
     if json_result is None:
         return routine_list
@@ -18,15 +15,11 @@ def get_active_routines(token):
         routine_list.append(new_routine)
     return routine_list
 
-def get_test_routine_as_list(token):
-    url = "http://localhost:8723/api/routine/test"
-    authorization_header = adapter_util.get_authorization_header(token)
-    r = requests.get(url, headers= authorization_header)
-    json_result = r.json()
+def get_test_routine_as_list(token):    
+    json_result = adapter_util.make_get_service_call("http://localhost:8723/api/routine/test", token)
     routine_list = []
     if json_result is None:
         return routine_list
     new_routine = Routine(json_result["RoutineId"], json_result["RoutinePatternDTOs"])
     routine_list.append(new_routine)
     return routine_list
-
--- RainIt/rain_it/rain_it/common/adapter_util.py
+++ RainIt/rain_it/rain_it/common/adapter_util.py
@@ -1,7 +1,31 @@
 '''
 This will include the common files
 '''
+import requests
+import json
+
 def get_authorization_header(token):
     header_value = "Bearer " + token
     authorization_header = {"Authorization": header_value}
     return authorization_header
+
+def is_json(json_data):
+    try:
+        json_object = json.loads(json_data)
+    except ValueError as  e:
+        return False
+    return True
+
+def make_get_service_call(url, token):
+    authorization_header = get_authorization_header(token)    
+    r = requests.get(url, headers= authorization_header)
+    json_result = r.json()
+    return json_result
+
+def make_post_service_call(url, token, data):
+    authorization_header = get_authorization_header(token)
+    if not data == "" and is_json(data):
+        authorization_header["content-type"]="application/json"
+    r = requests.post(url, headers= authorization_header, data = data)
+    json_result = r.json()
+    return json_result
