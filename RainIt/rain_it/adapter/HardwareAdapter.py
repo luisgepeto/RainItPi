60974c3ec508812cc2ebcc7522ab82fc5be98e3f
luisgepeto <luisgepeto@gmail.com>
2016-06-11 12:31:26 -0500

    added funcgion to get serial from file

--- RainIt/rain_it/adapter/HardwareAdapter.py
+++ RainIt/rain_it/adapter/HardwareAdapter.py
@@ -4,4 +4,13 @@ class HardwareAdapter(object):
         pass
 
     def get_serial(self):
-        return "0123456789abcdef"
+        cpu_serial = "0000000000000000"
+        try:
+            f = open('/proc/cpuinfo', 'r')
+            for line in f:
+                if line[0:6] == 'Serial':
+                    cpu_serial = line[10:26]
+            f.close()
+        except:
+            cpu_serial = "0123456789ABCDEF"
+        return cpu_serial
